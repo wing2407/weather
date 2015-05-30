c21a57d875366b5526c84553412d92a29402a88b
wing2407 <wing2407@sina.com>
2015-05-31 02:52:30 +0800

    新增数据库帮助类，以及各表对应实体类

---
+++ src/com/weather/app/model/Province.java
@@ -0,0 +1,29 @@
+package com.weather.app.model;
+
+public class Province {
+	
+	private int id;
+	private String provinceName;
+	private String ProvinceCode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public String getProvinceCode() {
+		return ProvinceCode;
+	}
+	public void setProvinceCode(String provinceCode) {
+		ProvinceCode = provinceCode;
+	}
+	
+	
+	
+}
